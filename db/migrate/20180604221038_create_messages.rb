3fb6f1f1dbad49dee5ef8002a8decc75c3ac67de
ckashby <clay.ashby@gmail.com>
2018-06-04 15:35:07 -0700

    Created conversation model

--- db/migrate/20180604221038_create_messages.rb
+++ db/migrate/20180604221038_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
     	t.text :body
     	t.references :conversation, index: true
+    	t.references :user, index: true
 
     	t.timestamps
     end
